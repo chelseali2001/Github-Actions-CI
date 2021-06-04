f9024e857b0df5cb7bd9419523307e9b1533e155
Chelsea Li <chelseali2001@gmail.com>
2021-06-04 01:43:55 -0700

    added tests for subtraction function

--- test.py
+++ test.py
@@ -7,5 +7,8 @@ class SimpleTest(unittest.TestCase):
         self.assertEqual(example.add(4, 2), 6)
 
 
+    def test_calculator(self):
+        self.assertEqual(example.sub(4, 2), 2)
+
 if __name__ == '__main__':
     unittest.main()
