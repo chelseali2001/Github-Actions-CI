60574f5dd880c63df8b7786e776ff7fa4e306f30
Chelsea Li <chelseali2001@gmail.com>
2021-06-04 01:41:15 -0700

    re-fixed lint errors

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import unittest
 import example
 
+
 class SimpleTest(unittest.TestCase):
     def test_calculator(self):
-        self.assertEqual(example.add(4, 2), 6) 
+        self.assertEqual(example.add(4, 2), 6)
+
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
