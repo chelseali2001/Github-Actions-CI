9356a326068a3d77c84f7b2e4084461bc714c385
Chelsea Li <chelseali2001@gmail.com>
2021-06-04 01:49:33 -0700

    added more tests and functions

--- test.py
+++ test.py
@@ -6,9 +6,15 @@ class SimpleTest(unittest.TestCase):
     def test_calculator(self):
         self.assertEqual(example.add(4, 2), 6)
 
-
-    def test_calculator(self):
+    def test_calculator0(self):
         self.assertEqual(example.sub(4, 2), 2)
+
+    def test_calculator1(self):
+        self.assertEqual(example.mul(4, 2), 8)
+
+    def test_calculator2(self):
+        self.assertEqual(example.div(4, 2), 2)
+
 
 if __name__ == '__main__':
     unittest.main()
